2b57c4177d49ab75f0bb715179eb99ed1704be71
Jaelapeno <64115028+Jaelapeno@users.noreply.github.com>
2020-06-24 12:45:20 +0800

    Updated

---
+++ 28_Jerome_PA02/Assets/_Obstacles/ObstacleCollision.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObstacleCollision : MonoBehaviour
+{
+    public GameObject obstacle;
+    public GameObject obstacle1;
+    public GameObject obstacle2;
+    public GameObject obstacle3;
+
+    private int rand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Obstacle", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rand = Random.Range(0, 5);
+    }
+
+    void Obstacle()
+    {
+        if (rand == 1)
+        {
+            Instantiate(obstacle,new Vector3(Random.Range(-5, 5), 0,0),Quaternion.identity);
+        }
+
+        if (rand == 2)
+        {
+            Instantiate(obstacle1,new Vector3(Random.Range(-5, 5), 0,0),Quaternion.identity);
+        }
+
+        if (rand == 3)
+        {
+            Instantiate(obstacle2, new Vector3(Random.Range(-5, 5), 0,0),Quaternion.identity);
+        }
+
+        if (rand == 4)
+        {
+            Instantiate(obstacle3,new Vector3(Random.Range(-5,5),0,0),Quaternion.identity);
+        }
+    }
+}
---
+++ 28_Jerome_PA02/Assets/_Player/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController thisController;
+    [SerializeField] private float JumpValue = 10;
+    [SerializeField] private float Gravity = 10;
+
+    private bool Jump = false;
+    private Vector3 MoveDirection = Vector3.zero;
+    private Transform playerMesh = null;
+    private Animator thisAnimator = null;
+    public GameObject explosion;
+    
+
+    private float moveSpeed = 0.05f;
+
+    void Start()
+    {
+        thisController = GetComponent<CharacterController>();
+        thisAnimator = GetComponentInChildren<Animator>();
+        playerMesh = transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        if (!Jump)
+        {
+            if (Input.GetKey(KeyCode.Space))
+                Jump = true;
+
+            if (thisController.isGrounded)
+            {
+                float MoveX = Input.GetAxis("Horizontal") * moveSpeed;
+                MoveDirection = transform.right * MoveX;
+
+                float AngleZ = transform.eulerAngles.z - (MoveX * 50000 * Time.deltaTime);
+                AngleZ = Mathf.Clamp(AngleZ, -45, 45);
+                playerMesh.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, AngleZ);
+            }
+
+            MoveDirection.y -= Gravity * Time.deltaTime;
+
+            if(GameManager.Lives == 0)
+            {
+                HUD.HUDManager.GameOver();
+            }
+        }
+
+        else
+        {
+            if (transform.position.y >= 0.25f)
+                Jump = false;
+            else
+                MoveDirection.y += JumpValue * Time.deltaTime;
+        }
+
+        thisController.Move(MoveDirection);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1.5f, 1.5f), transform.position.y, transform.position.z);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            GameManager.Lives -= 1;
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            HUD.HUDManager.UpdateLives();
+        }
+
+        if(other.gameObject.tag == "Score")
+        {
+            GameManager.Score += 1;
+            HUD.HUDManager.UpdateScore();
+        }
+    }
+}
